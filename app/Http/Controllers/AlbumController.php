c3bb01b655f2b0497c7a7d8af02580daf7867004
Sebastian Ospina <55632072+JSebastianOspina@users.noreply.github.com>
2020-12-10 20:18:58 -0600

    Added orderered images

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -16,12 +16,23 @@ class AlbumController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
 
-        $albumes = Auth::user()->hasRole('admin') ? Album::all() : Auth::user()->albums;
-
+        $albumes = Auth::user()->hasRole('admin') ? Album::where('url', 'Listo para descargar')->get() : Auth::user()->albums;
+        if ($request->todos == "true") {
+            $albumes = Auth::user()->hasRole('admin') ? Album::all() : Auth::user()->albums;
+        }
         return view('panel.album.clientes', compact('albumes'));
+    }
+    public function faq()
+    {
+        $todos = Album::where('url', 'no')->get();
+        foreach ($todos as $album) {
+            $album->url = "Listo para descargar";
+            $album->save();
+        }
+        return view('panel.facs');
     }
 
     public function verAlbum($id)
@@ -29,22 +40,34 @@ class AlbumController extends Controller
         $aux = str_replace('-', '/', $id);
         $directorio = "images/" . $aux;
         $album = Album::where('identificador', $id)->first();
-        $images = \File::Files($directorio);
+        $images = explode(',', $album->orden);
+        //$images = \File::Files($directorio);
+        //natsort($images);
         return view('panel.album.album', compact('images', 'directorio', 'album'));
     }
 
     function guardarAlbum(Request $request)
     {
+        $aux = $request->get('album');
+        $directorio = "images/" .   $request->get('user') . "/" . $aux;
 
-        $aux = Auth::user()->albums->count() + 1;
-        $directorio = "images/" . Auth::user()->id . "/" . $aux;
         if (Storage::exists($directorio)) {
+            $album = Album::where('identificador', $request->get('user') . '-' . $aux)->first();
+            if ($album != null) {
+                $album->url = "Listo para descargar";
+                $album->orden = $request->orden;
+                $album->cantidad =  count(\File::allFiles($directorio));
+
+                $album->save();
+                return response(route('album.ver', ['id' => Auth::user()->id . "-" . $aux]), 200);
+            }
             $album = new Album();
             $album->portada = $directorio . "/portada.jpeg";
             $album->cantidad =  count(\File::allFiles($directorio));
             $album->user_id = Auth::user()->id;
             $album->identificador = Auth::user()->id . "-" . $aux;
-
+            $album->url = "Listo para descargar";
+            $album->orden = $request->orden;
             $album->save();
             return response(route('album.ver', ['id' => Auth::user()->id . "-" . $aux]), 200);
         } else {
